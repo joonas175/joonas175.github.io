09cc8196634f494712976e883ed9c5b82e7f9d2a
joonas175 <36733885+joonas175@users.noreply.github.com>
2022-09-13 13:35:34 +0300

    Remove unused routes

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -114,12 +114,6 @@
 			<li class:active={$page.url.pathname === '/'}>
 				<a href="/">Home</a>
 			</li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a href="/todos">Todos</a>
-			</li>
 		</ul>
 	</nav>
 </header>
