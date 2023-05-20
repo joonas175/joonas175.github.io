e8cb3afbe9430033f7cbd6d13b468d424d5e7060
joonas175 <salojarvi.joonas@gmail.com>
2023-05-20 12:05:13 +0300

    Add prefetch, change bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), prefetch()],
 });
