6f2856012dd35f77bfcd0a2f35a6bfff4986e452
joonas175 <salojarvi.joonas@gmail.com>
2023-05-21 10:05:48 +0300

    Add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import prefetch from "@astrojs/prefetch";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), prefetch()],
+  integrations: [svelte(), prefetch(), vue()]
 });
