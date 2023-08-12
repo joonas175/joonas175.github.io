ba37409ccab4022013e7c841f9896b011bbff62f
joonas175 <salojarvi.joonas@gmail.com>
2023-08-12 14:08:45 +0300

    Add filters logic

--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -3,15 +3,21 @@
     <div class="filters">
       <b class="filters-title">FILTER BY</b>
       <div class="filter-container">
-        languages:
-        <b v-for="(on, lang) of langFilters" :class="{active: on}" @click="toggleFilter(lang, 'lang')">
+        languages
+        <b v-for="(on, lang) of langFilters" :class="{active: on}" @click="langFilters[lang] = !langFilters[lang];">
           {{ lang }}
         </b>
       </div>
       <div class="filter-container">
-        frameworks:
-        <b v-for="(on, fw) of fwFilters" :class="{active: on}" @click="toggleFilter(fw, 'fw')">
+        frameworks
+        <b v-for="(on, fw) of fwFilters" :class="{active: on}" @click="fwFilters[fw] = !fwFilters[fw];">
           {{ fw }}
+        </b>
+      </div>
+      <div class="filter-container">
+        project_type
+        <b v-for="(on, type) of typeFilters" :class="{active: on}" @click="typeFilters[type] = !typeFilters[type];">
+          {{ type }}
         </b>
       </div>
       <hr>
@@ -27,7 +33,7 @@
 </template>
 <script setup lang="ts">
 import type { CollectionEntry } from 'astro:content';
-import { computed, reactive } from 'vue';
+import { ComputedRef, computed, reactive } from 'vue';
 import ProjectListItem from './ProjectListItem.vue';
 
 const props = withDefaults(defineProps<{
@@ -49,23 +55,42 @@ const createFilterRecordFromSet = (collection: Iterable<string>) => {
 
 const langFilters: FilterRecord = reactive(createFilterRecordFromSet([...props.langs].sort()));
 const fwFilters: FilterRecord = reactive(createFilterRecordFromSet([...props.frameworks].sort()));
+const projectTypes = ['school', 'work', 'hobby', 'other'];
+const typeFilters: FilterRecord =  reactive(createFilterRecordFromSet(projectTypes));
 
-const filteredProjects = computed(() => {
-  return props.projects;
+const filteredProjects: ComputedRef<CollectionEntry<'project'>[]> = computed(() => {
+  const projects = new Set<CollectionEntry<'project'>>();
+  let hasFilters = false;
+  for(const lf in langFilters) {
+    if(langFilters[lf]) {
+      hasFilters = true;
+      props.projects.filter((p) => p.data.languages?.find((lang) => lang === lf)).forEach((p) => projects.add(p));
+    }
+  }
+
+  for(const fw in fwFilters) {
+    if(fwFilters[fw]) {
+      hasFilters = true;
+      props.projects.filter((p) => p.data.frameworks?.find((fw1) => fw1 === fw)).forEach((p) => projects.add(p));
+    }
+  }
+
+  return hasFilters ? [...projects] : props.projects;
 });
+
+const typeFilteredProjects: ComputedRef<CollectionEntry<'project'>[]> = computed(() => {
+  const activeTypeFilters: typeof projectTypes = [];
+  for(const type in typeFilters) {
+    if(typeFilters[type]) {
+      activeTypeFilters.push(type);
+    }
+  }
+  return activeTypeFilters.length === 0 ? filteredProjects.value : filteredProjects.value.filter((p) => activeTypeFilters.find((t) => t === p.data.type));
+})
 
 const sortedProjects = computed(() => {
-  return filteredProjects.value;
+  return typeFilteredProjects.value;
 });
-
-type FilterType = 'lang' | 'fw';
-const toggleFilter = (key: string, type: FilterType) => {
-  if(type === 'lang') {
-    langFilters[key] = !langFilters[key];
-  } else if(type === 'fw') {
-    fwFilters[key] = !fwFilters[key];
-  }
-}
 
 </script>
 
@@ -84,6 +109,7 @@ const toggleFilter = (key: string, type: FilterType) => {
 
 .filters-title {
   align-self: center;
+  margin-bottom: 5px;
 }
 
 .filter-container {
@@ -92,11 +118,11 @@ const toggleFilter = (key: string, type: FilterType) => {
   flex-wrap: wrap;
 
   b {
-    padding: 3px 6px;
+    padding: 2px 6px;
     cursor: pointer;
     border: 1px solid;
     border-radius: 2px;
-    margin: 3px;
+    margin: 4px;
 
     &.active {
       color: var(--highlight-text-color);
