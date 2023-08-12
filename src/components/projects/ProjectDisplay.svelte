33bda0a29cef58240ec62b01b20641542ee01388
joonas175 <salojarvi.joonas@gmail.com>
2023-08-12 13:03:39 +0300

    Add container for displaying single project

---
+++ src/components/projects/ProjectDisplay.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+<div class="project-display-container">
+  <a href="/projects/" rel="prefetch">Back to projects</a>
+  <div class="project-display-content">
+    <slot></slot>
+  </div>
+  <a href="/projects/" rel="prefetch">Back to projects</a>
+</div>
+
+<style lang="scss">
+.project-display-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.project-display-content {
+  width: 100%;
+}
+</style>
