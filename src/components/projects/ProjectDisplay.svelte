b459212d8fefb8f0cba3c71d2552281d95f023c8
joonas175 <salojarvi.joonas@gmail.com>
2023-08-23 19:38:02 +0300

    Navigate back instead of projects list

--- src/components/projects/ProjectDisplay.svelte
+++ src/components/projects/ProjectDisplay.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  const goBack = () => {
+    history.back();
+  }
 </script>
 <div class="project-display-container">
-  <a href={"#"} on:click|preventDefault={history.back}>Back</a>
+  <a href={"#"} on:click|preventDefault={goBack}>Back</a>
   <div class="project-display-content">
     <slot></slot>
   </div>
-  <a href={"#"} on:click|preventDefault={history.back}>Back</a>
+  <a href={"#"} on:click|preventDefault={goBack}>Back</a>
 </div>
 
 <style lang="scss">
